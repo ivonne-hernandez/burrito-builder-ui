e36519add882b23de55ab1614eb958f6c07b0d44
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-19 09:56:00 -0700

    Fix the bug in the Orders.js file by adding keys and id

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+const Orders = ({ orders }) => {
+  const orderEls = orders.map(order => {
     return (
-      <div className="order">
+      <div 
+        className="order"
+        key={order.id}
+        id={order.id}>
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
+            return <li key={ingredient}>
+                    {ingredient}
+                    </li>
           })}
         </ul>
       </div>
@@ -19,7 +24,7 @@ const Orders = props => {
     <section>
       { orderEls.length ? orderEls : <p>No orders yet!</p> }
     </section>
-  )
+  );
 }
 
 export default Orders;
