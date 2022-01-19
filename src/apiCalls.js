48beeec6cf807e2eb1cbe62757aa377ce032a53d
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-19 09:21:38 -0700

    Add addOrder function to apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,6 +8,23 @@ const getOrders = () => {
       })
 }
 
+const addOrder = (newOrder) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    body: JSON.stringify(newOrder),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+      return response.json();
+    })
+}
+
 module.exports = {
-  getOrders
+  getOrders,
+  addOrder
 }
