f06f279a284171a09e22990ad8f3593b45fd8132
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-19 09:57:43 -0700

    Connect the front end to the back end via componentDidMount method and getOrders function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import {getOrders, addOrder, deleteExistingOrder} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
-    getOrders()
+    return getOrders()
+      .then(data => {
+        this.setState({ orders: data.orders })
+      })
       .catch(err => console.error('Error fetching:', err));
+  }
+
+  addOrder = (order) => {
+    
   }
 
   render() {
@@ -21,12 +31,10 @@ class App extends Component {
           <h1>Burrito Builder</h1>
           <OrderForm />
         </header>
-
         <Orders orders={this.state.orders}/>
       </main>
     );
   }
 }
-
 
 export default App;
